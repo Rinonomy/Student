1f71f7f845ec1ca792694f776e03418897fff87f
Rinonomy <maniaklovelyyy@gmail.com>
2024-10-07 23:56:39 +0300

    Add files via upload

---
+++ Student/src/Student.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+class Student {
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+}
+
